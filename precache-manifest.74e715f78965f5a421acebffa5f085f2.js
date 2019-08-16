af464966cc1185c6cf1c5c56a112e701e3feb67a
aceetheridge <aceetheridge@gmail.com>
2019-08-16 17:17:57 -0400

    Updates

---
+++ precache-manifest.74e715f78965f5a421acebffa5f085f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "819e23c17b7a3b0d62c8088e44cc5ad9",
+    "url": "/Image-Detection-Website/index.html"
+  },
+  {
+    "revision": "42b37239ae877dc4aafb",
+    "url": "/Image-Detection-Website/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "3358e3920f86cc14780b",
+    "url": "/Image-Detection-Website/static/css/main.aebdd2fa.chunk.css"
+  },
+  {
+    "revision": "42b37239ae877dc4aafb",
+    "url": "/Image-Detection-Website/static/js/2.9f258a84.chunk.js"
+  },
+  {
+    "revision": "3358e3920f86cc14780b",
+    "url": "/Image-Detection-Website/static/js/main.8c303241.chunk.js"
+  },
+  {
+    "revision": "094f7beb2e552f980c23",
+    "url": "/Image-Detection-Website/static/js/runtime~main.c21f9619.js"
+  }
+]);
